ac5e326153a3b2a62329d885bee9a08156f39d3c
Andrew Moss <a.moss@sobolt.com>
2023-07-16 11:13:03 +0200

    part 2 implemented

--- src/lib.rs
+++ src/lib.rs
@@ -22,6 +22,67 @@ pub fn process_part_1(input: &str) -> usize {
     coords.len()
 }
 
+pub fn process_part_2(input: &str) -> usize {
+    let mut highest_scenic_score: usize = 0;
+    let grid = get_grid_from_input(input);
+    for j in 0..grid.len() {
+        for i in 0..grid[0].len() {
+            let score = calculate_coord_scenic_score(&grid, (i, j));
+            if score > highest_scenic_score {
+                highest_scenic_score = score;
+            }
+        }
+    }
+    highest_scenic_score
+}
+
+fn get_grid_from_input(input: &str) -> Vec<Vec<u32>> {
+    let mut grid: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| extract_row_of_heights(line))
+        .collect();
+    grid.reverse();
+    grid
+}
+
+fn calculate_coord_scenic_score(grid: &Vec<Vec<u32>>, coord: (usize, usize)) -> usize {
+    let original_tree_height = grid[coord.1][coord.0];
+
+    let mut score_west: usize = 0;
+    for i in (0..coord.0).rev() {
+        score_west += 1;
+        if grid[coord.1][i] >= original_tree_height {
+            break;
+        }
+    }
+
+    let mut score_east: usize = 0;
+    for i in coord.0 + 1..grid.len() {
+        score_east += 1;
+        if grid[coord.1][i] >= original_tree_height {
+            break;
+        }
+    }
+
+    let mut score_south: usize = 0;
+    for j in (0..coord.1).rev() {
+        score_south += 1;
+        if grid[j][coord.0] >= original_tree_height {
+            break;
+        }
+    }
+
+    let mut score_north: usize = 0;
+    for j in coord.1 + 1..grid[0].len() {
+        score_north += 1;
+        if grid[j][coord.0] >= original_tree_height {
+            break;
+        }
+    }
+    let scenic_score = score_east * score_north * score_south * score_west;
+    scenic_score
+}
+
 fn extract_row_of_heights(row: &str) -> Vec<u32> {
     let heights: Vec<u32> = row.chars().map(|c| c.to_digit(10).unwrap()).collect();
     heights
@@ -61,6 +122,41 @@ fn get_visible_tree_idxs(row: &Vec<u32>) -> HashSet<usize> {
 mod tests {
     use super::*;
     use std::fs;
+
+    #[test]
+    fn test_process_part_2() {
+        let input = fs::read_to_string("test-input1.txt").unwrap();
+        let result = process_part_2(&input);
+        assert_eq!(result, 8);
+    }
+
+    #[test]
+    fn test_get_grid_from_input() {
+        let input = fs::read_to_string("test-input1.txt").unwrap();
+        let result = get_grid_from_input(&input);
+        assert_eq!(
+            result,
+            vec![
+                vec![3, 5, 3, 9, 0],
+                vec![3, 3, 5, 4, 9],
+                vec![6, 5, 3, 3, 2],
+                vec![2, 5, 5, 1, 2],
+                vec![3, 0, 3, 7, 3],
+            ]
+        );
+    }
+
+    #[test]
+    fn test_calculate_scenic_score() {
+        let input = fs::read_to_string("test-input1.txt").unwrap();
+        let grid = get_grid_from_input(&input);
+
+        let result = calculate_coord_scenic_score(&grid, (2, 1));
+        assert_eq!(result, 8);
+
+        let result = calculate_coord_scenic_score(&grid, (2, 3));
+        assert_eq!(result, 4);
+    }
 
     #[test]
     fn text_transpose_grid() {
--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,17 @@ use std::fs;
 
 fn main() {
     part_1();
+    part_2();
 }
 
 fn part_1() {
     let input = fs::read_to_string("input.txt").unwrap();
     let result = aoc_8::process_part_1(&input);
     println!("Part1: {}", result);
+}
+
+fn part_2() {
+    let input = fs::read_to_string("input.txt").unwrap();
+    let result = aoc_8::process_part_2(&input);
+    println!("Part2: {}", result);
 }
